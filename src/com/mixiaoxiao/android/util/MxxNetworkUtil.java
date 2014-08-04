ab59f69fac7c7f138543a97cda707ddb0261d956
bushijie33@gmail.com <bushijie33>
2014-08-04 13:57:30 +0800

    git 9gag客户端项目

---
+++ src/com/mixiaoxiao/android/util/MxxNetworkUtil.java
@@ -0,0 +1,44 @@
+package com.mixiaoxiao.android.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class MxxNetworkUtil {
+	
+	public static boolean isNetworkAvailable(Context context) {
+		ConnectivityManager connectivity = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (connectivity == null) {
+			return false;
+		} else {
+			NetworkInfo[] info = connectivity.getAllNetworkInfo();
+			if (info != null) {
+				for (int i = 0; i < info.length; i++) {
+					if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 王斌增加，用于判断是否是wifi 网络
+	 * 
+	 * @param context
+	 * @return 是否WIFI网络
+	 */
+	public static boolean isWifiConnect(Context context) {
+		ConnectivityManager cm = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkINfo = cm.getActiveNetworkInfo();
+		if (networkINfo != null
+				&& networkINfo.getType() == ConnectivityManager.TYPE_WIFI) {
+			return true;
+		}
+		return false;
+	}
+
+}
